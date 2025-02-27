2323ea13c07e1e426ad85c25423439d263985852
Jan Kudrna <jan.kudrna.tt@gmail.com>
2025-02-27 16:47:04 +0100

    Uprava Navbaru, pridani sekci

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,16 +5,22 @@ const Navbar = () => {
     <nav className="fixed top-0 left-0 w-full bg-black bg-opacity-50 backdrop-blur-md py-4 px-6 flex justify-between items-center z-50">
       {/* Logo */}
       <div className="text-white text-xl font-bold">
-        <Link href="/">🌿 Pralesy</Link>
+        <Link href="/">🌿 Terra Verde</Link>
       </div>
 
       {/* Navigační odkazy */}
-      <div className="space-x-6">
+      <div className="space-x-6 hidden md:flex"> {/* Skryté na mobilech, viditelné na větších obrazovkách */}
         <Link className="text-white hover:text-green-400 transition" href="/pralesy">
           Pralesy
         </Link>
-        <Link className="text-white hover:text-green-400 transition" href="/about">
-          O webu
+        <Link className="text-white hover:text-green-400 transition" href="/ekologie">
+          Ekologie
+        </Link>
+        <Link className="text-white hover:text-green-400 transition" href="/cestovani">
+          Cestování
+        </Link>
+        <Link className="text-white hover:text-green-400 transition" href="/blog">
+          Blog
         </Link>
         <Link className="text-white hover:text-green-400 transition" href="/contact">
           Kontakt
